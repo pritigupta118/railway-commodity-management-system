bad30cfc492d68a718140d9243f061883945ff21
Priti Gupta <civilstudent9900@gmail.com>
2024-06-11 22:39:09 +0530

    create user model

---
+++ src/models/user.model.js
@@ -0,0 +1,85 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true
+  },
+  phone: {
+    type: String,
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"]
+  },
+  refreshToken: {
+    type: String
+  },
+  is_admin: {
+    type: Boolean
+  }
+});
+
+// encrypting password
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
+
+
+// creating a custom methos to check the password
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+//generating access token
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id : this._id,
+      email : this.email,
+      fullName : this. fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+//generating refresh token
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn : process.env.REFRESH_TOKEN_SECRET
+    }
+  )
+}
+
+export const User = mongoose.model("User", userSchema)
